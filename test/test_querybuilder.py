a2db7857e356409eef9012ed2f5adf89ebd6ef1e
ferantivero <fantivero@gmail.com>
2015-10-29 10:49:19 -0300

    change unit test method name for a better one, start creating db mock instance and +1 ut
    change ut name
    start defining the strategy of db mocking for ut purposes
    add one ut that test the creation of a new and valid QueryBuilder instance

--- test/test_querybuilder.py
+++ test/test_querybuilder.py
@@ -1,9 +1,20 @@
 import unittest
+from unittest.mock import Mock,MagicMock
 from pyquebec.querybuilder import QueryBuilder
 
 class TestQueryBuilder(unittest.TestCase):
-    def test_where_with_None_value(self):
+    @classmethod
+    def setUpClass(cls):
+        dbo_mock = Mock()
+        cls._mock_db_instance = Mock()
+        cls._mock_db_instance.dbo = MagicMock(return_value=dbo_mock)
+
+    def test_querybuilderctor_nonedbinstance_then_raises_ValueError(self):
         self.assertRaises(ValueError, QueryBuilder,None)
 
+    def test_querybuilderctor_validdbinstance_the_createavalidinstance(self):
+        qb = QueryBuilder(self._mock_db_instance)
+        self.assertIsNotNone(qb)
+    
 if __name__ == '__main__':
     unittest.main()
