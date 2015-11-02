89669364aff11a6c510647714a4eefe428df388c
ferantivero <fantivero@gmail.com>
2015-11-02 07:52:52 -0300

    +1 unit tests and improve mocked db instance
    add new unit test for where
    improve the mocked db instance. Now it is a valid one (schema,table and colums are available)

--- test/test_querybuilder.py
+++ test/test_querybuilder.py
@@ -1,13 +1,18 @@
 import unittest
 from unittest.mock import Mock,MagicMock
 from pyquebec.querybuilder import QueryBuilder
+from pyquebec.dbobjects import Table
 
 class TestQueryBuilder(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        dbo_mock = Mock()
-        cls._mock_db_instance = Mock()
-        cls._mock_db_instance.dbo = MagicMock(return_value=dbo_mock)
+        mock_db = Mock()
+        mock_schema = Mock()
+        mock_schema.schema_name = 'foo'
+        mock_db.foo = mock_schema
+        mock_table = Table(mock_db, mock_schema,'bar',['id','desc'])
+        mock_db.foo.bar = mock_table   
+        cls._mock_db_instance = mock_db
 
     def test_querybuilderctor_nonedbinstance_then_raises_ValueError(self):
         self.assertRaises(ValueError, QueryBuilder,None)
@@ -15,6 +20,12 @@ class TestQueryBuilder(unittest.TestCase):
     def test_querybuilderctor_validdbinstance_the_createavalidinstance(self):
         qb = QueryBuilder(self._mock_db_instance)
         self.assertIsNotNone(qb)
-    
+
+    def test_querybuilderwhere_wherewithnone_then_raises_ValueError(self):
+        qb = QueryBuilder(self._mock_db_instance)
+        qb.From(self._mock_db_instance.foo.bar)
+        self.assertRaises(ValueError, qb.where)
+
 if __name__ == '__main__':
     unittest.main()
+
