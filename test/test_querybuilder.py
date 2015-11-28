f8cf6d9931afe6f6bb95e32149f3660af5fce67b
Sebastián Monía <seb.hoagie@outlook.com>
2015-11-28 16:50:25 -0700

    Use a cached schema and monkeypatching to test
    Monkey patches the schema reader and uses a cached schema to test
    QueryBuilder

--- test/test_querybuilder.py
+++ test/test_querybuilder.py
@@ -1,37 +1,50 @@
 import unittest
 from unittest.mock import Mock, MagicMock
 from pyquebec.querybuilder import QueryBuilder
-from pyquebec.dbobjects import Table
+import os
+import pyquebec.database
+import pyquebec.schema_reader as schema_reader
+import pyquebec
 
 class TestQueryBuilder(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        mock_db = Mock()
-        mock_db.config = Mock()
-        mock_db.config.uses_schema = True
+        pyquebec.database.get_db_config = Mock()
+        schema_reader._cache_folder = os.path.split(__file__)[0]
+        test_schema = pyquebec.schema_reader.read_schema_from_cache('Chinook_Test')
+        test_db = pyquebec.database.DataBase('Test', test_schema)
+        test_db.config = Mock()
+        test_db.config.uses_schema = False
+        test_db.db_name = 'Test'
         query_templates = {}
         query_templates['select'] = "SELECT {0}"
-        query_templates['from'] = "FROM {0} WITH (NOLOCK)"
+        query_templates['from'] = "FROM {0}"
         query_templates['inner_join'] = "INNER JOIN {0} ON {1} = {2}"
         query_templates['left_join'] = "LEFT JOIN {0} ON {1} = {2}"
         query_templates['where'] = "WHERE {0}"
         query_templates['order_by'] = "ORDER BY {0}"
-        mock_db.config.query_templates = query_templates
-        mock_schema = Mock()
-        mock_schema.schema_name = 'dbo'
-        mock_db.dbo = mock_schema
-        mock_table = Table(mock_db, mock_schema,'Table1',['Table1_id','Table1_description'])
-        mock_db.dbo.Table1 = mock_table
-        mock_table = Table(mock_db, mock_schema,'Table2',['Table2_id','Table2_description', 'Table1_id'])
-        mock_db.foo.Table2 = mock_table
-        cls._mock_db_instance = mock_db
+        test_db.config.query_templates = query_templates
+        cls._test_db = test_db
 
     def test_querybuilderctor_nonedbinstance_then_raises_ValueError(self):
         self.assertRaises(ValueError, QueryBuilder,None)
 
     def test_querybuilderctor_validdbinstance_then_createavalidinstance(self):
-        qb = QueryBuilder(self._mock_db_instance)
+        qb = QueryBuilder(self._test_db)
         self.assertIsNotNone(qb)
+
+    def test_querybuilderwhere_wherestring_then_appends_string(self):
+        qb = QueryBuilder(self._test_db)
+        qb.From(self._test_db.Album).where('1 = 1')
+        query = qb.preview()
+        expected = "SELECT * FROM Album  WHERE 1 = 1 "
+        self.assertEqual(query, expected)
+
+    def test_querybuilder_fromtable_where_wherestring_then_appends_string(self):
+        qb = self._test_db.Album.From().where('1 = 1')
+        query = qb.preview()
+        expected = "SELECT * FROM Album  WHERE 1 = 1 "
+        self.assertEqual(query, expected)
 
     # def test_querybuilderwhere_wherewithnone_then_raises_ValueError(self):
     #     qb = QueryBuilder(self._mock_db_instance)
